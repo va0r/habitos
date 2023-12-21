f5be684a357a31d91fc3f6ae6b93d430fee1bee4
va0r <i@vialor.ru>
2023-12-21 19:04:56 +0300

    initial commit

---
+++ users/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from constants import NULLABLE
+
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'member', _('Member')
+    MODERATOR = 'moderator', _('Moderator')
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name=_('Email'))
+    telegram_id = models.CharField(max_length=255, verbose_name=_('Telegram ID'), **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name=_('Phone'), **NULLABLE)
+    city = models.CharField(max_length=150, verbose_name=_('City'), **NULLABLE)
+    avatar = models.ImageField(upload_to='user/', verbose_name=_('Avatar'), **NULLABLE)
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.MEMBER)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
