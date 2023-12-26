79117741ccede0f5bc6e2a0d2457030a216138ef
va0r <i@vialor.ru>
2023-12-26 17:00:16 +0300

    code review

--- scripts/flake8.py
+++ scripts/flake8.py
@@ -2,5 +2,5 @@ import os
 
 if __name__ == "__main__":
 
-    executive_string = 'flake8 ../. --ignore=E501,F401,F811 > ../data/flake8_report.txt 2>&1'
+    executive_string = 'flake8 ../. --ignore=E501,F401,F811 > ../flake8_text_report/flake8_report.txt 2>&1'
     os.system(executive_string)
